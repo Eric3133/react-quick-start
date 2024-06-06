d5a295f4d5257ddfac7100e82fd3dda92405c0a0
mahongri <mahongri1993@gmail.com>
2024-06-06 00:40:52 -0400

    describe the UI - your first conponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+export function Profile () {
+  return (
+    <img src= "https://i.imgur.com/yXOvdOSs.jpg" alt= ''/>
+  )
+};
+
+export function Gallery () {
+  return (
+  <section>
+    <h1>Amazing scientists</h1>
+    <Profile />
+    <Profile />
+    <Profile />
+  </section>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      {/* <Profile /> */}
+      <Gallery/>
     </div>
   );
 }
---
+++ src/why-react/readme.md
@@ -0,0 +1,5 @@
+# 计数器
+
+命令式编程： js jquery
+
+声明式编程 react, vue
