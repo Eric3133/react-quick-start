9cfbd1620bc696e67854a4f2434143ace8ae11ee
mahongri <mahongri1993@gmail.com>
2024-06-13 00:52:43 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
-import { Profile } from './Gallery.js';
-import Gallery from './Gallery.js';
+function Item({isPacked, name}) {
+  // if(isPacked) {
+  //   // return(
+  //   //   <li className="item packed">{name}✔</li>
+  //   // )
+  //   return null
+  // }
+  //   return(
+  //     <li className="item">{name}</li>
+  //   )
+  // return <li className="item">{isPacked? name + '✔' : name}</li>
+  // return <li className="item">
+  //   {isPacked ? (
+  //     <del>
+  //       {name + '✔'}
+  //     </del>
+  //   ):(
+  //     name
+  //   )}
+  // </li>
+  return <li className="item">
+    {name} {isPacked && '✔'}
+  </li>
+}
 
-export default function App() {
-  return(
-    <h1>Gallery</h1>,
-    <Gallery />,
-    <Profile />
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+           isPacked={true}
+           name="Space suit"
+        />
+
+        <Item
+           isPacked={true}
+           name="Helmet with a golden leaf"
+        />
+
+        <Item
+           isPacked={false}
+           name="Photo of Tam"
+        />
+      </ul>
+    </section>
   )
 }
